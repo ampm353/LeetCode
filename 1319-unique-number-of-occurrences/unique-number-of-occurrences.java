f524b223419aade239f188891baa3ae3a014c873
jinyoung.choi <49185800+realzero-choi@users.noreply.github.com>
2024-01-17 23:57:25 +0900

    Time: 2 ms (98.10%) | Memory: 41.4 MB (44.09%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -10,19 +10,8 @@ class Solution {
         int num = 0;
 
         // 중복된 값을 체크할 Set 생성
-        Set<Integer> uniqueValues = new HashSet<>();
-
-        // Map의 모든 값들을 얻어옴
-        Collection<Integer> values = map.values();
-
-        for (Integer value : values) {
-            // 중복된 값이 이미 Set에 있다면 중복이 있다고 판단
-            if (!uniqueValues.add(value)) {
-                System.out.println("중복된 값이 존재합니다: " + value);
-                // 원하는 추가적인 처리 수행
-                return false;
-            }
-        }
-        return true;
+        Set<Integer> values = new HashSet<>(map.values());
+        
+        return values.size() == map.size();
     }
 }
