bbf42b9a15889f82adeaa81e46d8a162e760ea06
jinyoung.choi <49185800+realzero-choi@users.noreply.github.com>
2024-01-25 12:54:29 +0900

    Time: 1 ms (73.66%) | Memory: 41.2 MB (18.84%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,36 +1,24 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         
-        // Create a list to store the elements of the current row in Pascal's Triangle
-        List<Integer> l = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
-        // Iterate through each row up to the specified rowIndex
-        for (int i = 0; i <= rowIndex; i++) 
-        {
-            // Create an array dp to represent the current row
-            int[] dp = new int[i + 1];
+        result.add(1);
 
-            // Set the first and last elements of the row to 1
-            dp[0] = 1;
-            dp[dp.length - 1] = 1;
+        for(int i = 1 ; i <= rowIndex; i ++){
+            List<Integer> list = new ArrayList<>();
 
-            // Calculate the values for the non-edge elements in the row
-            for (int j = 1; j < dp.length - 1; j++) 
-            {
-                // The value at position j is the sum of the two elements from the previous row
-                dp[j] = l.get(j - 1) + l.get(j);
+            list.add(1);
+
+            for(int j = 1 ; j < result.size() ; j ++){
+                list.add(result.get(j-1) + result.get(j));
             }
 
-            // Clear the list to prepare for the next row
-            l.clear();
+            list.add(1);
 
-            // Convert the array to a List and add it to the list l
-            for (int num : dp) {
-                l.add(num);
-            }
+            result = list;
         }
 
-        // Return the list representing the rowIndex-th row in Pascal's Triangle
-        return l;
+        return result;
     }
 }
