023537cffe24094c156b1aae5fea90981e5cfd7e
jinyoung.choi <49185800+realzero-choi@users.noreply.github.com>
2024-01-25 12:15:53 +0900

    Time: 7 ms (91.39%) | Memory: 69.6 MB (70.41%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -14,32 +14,27 @@
  * }
  */
 class Solution {
+    int count = 0;
     public int pseudoPalindromicPaths (TreeNode root) {
-        int count = 0, path = 0;
         
-        Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
-        stack.push(new Pair(root, 0));
-        while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> p = stack.pop();
-            TreeNode node = p.getKey();
-            path = p.getValue();
+        preorder(root, 0);
+        return count;
+    }
 
-            if (node != null) {
-                // compute occurrence of each digit 
-                // in the corresponding register
-                path = path ^ (1 << node.val);
-                // if it's a leaf check if the path is pseudo-palindromic
-                if (node.left == null && node.right == null) {
-                    // check if at most one digit has an odd frequency
-                    if ((path & (path - 1)) == 0) {
-                        ++count;
-                    }
-                } else {
-                    stack.push(new Pair(node.right, path));
-                    stack.push(new Pair(node.left, path));
+    public void preorder(TreeNode node, int path) {
+        if (node != null) {
+            // compute occurences of each digit 
+            // in the corresponding register
+            path = path ^ (1 << node.val);
+            // if it's a leaf check if the path is pseudo-palindromic
+            if (node.left == null && node.right == null) {
+                // check if at most one digit has an odd frequency
+                if ((path & (path - 1)) == 0) {
+                    ++count;
                 }
             }
+            preorder(node.left, path);
+            preorder(node.right, path) ;
         }
-        return count;
     }
 }
