c5a6b936797ac005266badaffabead625823eb23
jinyoung.choi <49185800+realzero-choi@users.noreply.github.com>
2024-02-01 12:47:38 +0900

    Time: 24 ms (68.69%) | Memory: 62.2 MB (5.21%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -5,18 +5,13 @@ class Solution {
 
         Arrays.sort(nums);
 
-        int index = 0;
         for(int i = 0 ; i < nums.length-2; i= i+3) {
             
             if(nums[i+2] - nums[i] > k){
                 return new int[0][0];
             }
 
-            result[index][0] = nums[i];
-            result[index][1] = nums[i+1];
-            result[index][2] = nums[i+2];
-
-            index++;
+            result[i/3] = new int[]{nums[i], nums[i+1], nums[i+2]};
         }
 
         return result;
